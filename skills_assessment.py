0381e79a47644908ca7bcaea1cf568fd2b049578
Sibusiso-Mgidi <mgiditercy@gmail.com>
2021-06-01 12:49:48 +0200

    added a question and solution class

--- skills_assessment.py
+++ skills_assessment.py
@@ -26,10 +26,10 @@ Polymorphism:
 - Are functions of objects
 """
 
-"""YOU ARE TASKED TO CREATE A SCRIPT FOR STUDENTS ASSESSMENT
-
-"""
 class  Student():
+    """
+    This is a blueprint fr getting the students demographic dataset.
+    """
     def __init__(self, first_name, last_name, student_number, course_name, address):
         self.first_name = first_name
         self.last_name = last_name
@@ -38,9 +38,47 @@ class  Student():
         self.address = address
 
     def __str__(self):
+        """
+        returns demographic data
+        """
         return "First name: {} \nLast name: {} \nStudent number: {} \nCourse: {} \nAddress: {}".format(self.first_name, self.last_name, self.student_number, self.course_name, self.address)
     
+class Question():
+    """
+    This is a blueprint for storing quetsions and solutions.
+    """
+    def __init__(self, question, solution):
+        self.question = question
+        self.solution = solution
 
+    def __str__(self):
+        """
+        This method returns the question and solution.
+        """
+        return "Question: {} \n Solution: {}".format(self.question, self.solution)
 
-student_details = Student("Sibusiso", "Mgidi", "1141573", "BSC. Honours in Computer Science", "Braamfontein")
-print(student_details)
+    def ask_and_evaluate(self):
+        """
+        This method prints thee questio and prompts the user to answer.
+        :returns: boolean values (True or False)
+        """
+        
+        print(self.question)
+        print("Please Type True or False only")
+        user_solution = input("Enter the solution: \n")
+        if user_solution == self.solution:
+            print("Your solutions is correct")
+            return True
+        else:
+            print("Your solutions is incorrect")
+            return False
+        
+        
+
+    
+# test the class
+question= Question("Was Sibusiso Mgidi born on the 2nd of January 1996: \n",True)
+
+question.ask_and_evaluate()
+
+print(question)
