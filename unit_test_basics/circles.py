70c94ac6103f458cce08ba984e846734ab3046f3
Sibusiso-Mgidi <mgiditercy@gmail.com>
2021-07-12 00:53:07 +0200

    added our first test case

---
+++ unit_test_basics/circles.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def get_circle_area(r):
+    
+    return np.pi * np.power(r, 2)
--- unit_test_basics/test_circles.py
+++ unit_test_basics/test_circles.py
@@ -1,6 +1,18 @@
 import unittest
 import numpy as np
+from circles import get_circle_area
 
-def get_circle_area(r):
-    
-    return np.pi * np.power(r, 2)
+class TestCircleArea(unittest.TestCase):
+    """
+    This is a class consists of test methods. Each method name must start with the name test.
+    """
+    def test_area(self):
+        # Test for area when the radius >= 0
+        self.assertAlmostEqual(get_circle_area(1), np.pi)
+        self.assertAlmostEqual(get_circle_area(0), 0)
+        self.assertAlmostEqual(get_circle_area(2.1), np.pi*np.power(2.1,2))
+        
+
+        
+
+
